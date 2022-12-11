aae44f370261ec8ca2f6418806a3fbc29984f5dd
FVargas4 <fvargascode@gmail.com>
2022-12-11 13:42:34 -0600

    Initial Commit

---
+++ AppTutorialProject/AppTutorialProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTutorialProjectApp.swift
+//  AppTutorialProject
+//
+//  Created by Fer Vargas on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AppTutorialProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
